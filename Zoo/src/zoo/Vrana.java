c6690f6537f4e42dd20b4ea224746e1cbe4ca689
vrana <vrana@PC542.SPSEJECNANET.CZ>
2019-06-17 08:52:09 +0200

    add class Vrana

---
+++ Zoo/src/zoo/Vrana.java
@@ -0,0 +1,37 @@
+package zoo;
+
+public class Vrana implements AnimalInterface{
+
+	@Override
+	public double getWeight() {
+		// TODO Auto-generated method stub
+		return 0.5;
+	}
+
+	@Override
+	public void setWeight(double weight) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public double foodConsumption() {
+		// TODO Auto-generated method stub
+		return 0.25;
+	}
+
+	@Override
+	public String doSound() {
+		// TODO Auto-generated method stub
+		return "caw caw";
+	}
+
+	@Override
+	public String whoAmI() {
+		// TODO Auto-generated method stub
+		return "BBB big black bird";
+	}
+	
+	
+
+}
